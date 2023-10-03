02fa2dd461261b976251fb9015c24785945ca3ba
Oleksandr Sheremet <o.sheremet@viseven.com>
2023-10-03 16:50:53 +0300

    update(header) add multilang and fix styles

---
+++ js/change-lang.js
@@ -0,0 +1,28 @@
+const select = document.querySelector("#select");
+const allLang = ["en", "ru", "ua"];
+select.addEventListener("change", changeURL);
+
+function changeURL() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#en";
+    location.reload();
+  }
+  select.value = hash;
+  document.querySelector("title").innerHTML = langArr["unit"][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector(".lng-" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+  }
+}
+changeLanguage();
---
+++ js/lang.js
@@ -0,0 +1,27 @@
+const langArr = {
+    "unit":{
+        "ru":"Онлайн фитнес тренер",
+        "en":"Online fitness trainer",
+        "ua":"Онлайн фітнес тренер"
+    },
+    "main":{
+        "ru":"Главная",
+        "en":"Main",
+        "ua":"Головна"
+    },
+    "programs":{
+        "ru":"Программы",
+        "en":"Programs",
+        "ua":"Програми"
+    },
+    "about-me":{
+        "ru":"Обо мне",
+        "en":"About me",
+        "ua":"Про мене"
+    },
+    "questions":{
+        "ru":"Вопросы",
+        "en":"Questions",
+        "ua":"Питання"
+    }
+}
