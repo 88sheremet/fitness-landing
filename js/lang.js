693f55b9894a4d97313c3cba1191fc33a5d38d75
Oleksandr Sheremet <o.sheremet@viseven.com>
2023-10-03 17:30:45 +0300

    update(lang.js): add multilang for directions section

--- js/lang.js
+++ js/lang.js
@@ -23,5 +23,56 @@ const langArr = {
         "ru":"Вопросы",
         "en":"Questions",
         "ua":"Питання"
-    }
+    },
+    "hero__title":{
+        "ru":"Онлайн фитнес тренер",
+        "en":"Online fitness trainer",
+        "ua":"Онлайн фітнес тренер"
+    },
+    "hero__title2":{
+        "ru":"Александр Шеремет",
+        "en":"Oleksandr Sheremet",
+        "ua":"Олександр Шеремет"
+    },
+    "hero__title3":{
+        "ru":"Стань лучшей версией себя!",
+        "en":"Become the best version of yourself!",
+        "ua":"Стань кращою версією себе!"
+    },
+    "main-btn":{
+        "ru":"Записаться",
+        "en":"Sign up for training",
+        "ua":"Записатись"
+    },
+    "directions":{
+        "ru":"Направления тренировок",
+        "en":"Training directions",
+        "ua":"Напрями тренувань"
+    },
+    "spine":{
+        "ru":"Спина без боли",
+        "en":"Back without pain",
+        "ua":"Спина без болю"
+    },
+    "spine-1":{
+        "ru":"Укрепление мышц спины",
+        "en":"Strengthening your back muscles",
+        "ua":"Зміцнення м'язів спини"
+    },
+    "spine-2":{
+        "ru":"Устранение различных болей в спине и шейном отделе",
+        "en":"Elimination of various back and cervical pains",
+        "ua":"Усунення різних болів у спині та шийному відділі"
+    },
+    "spine-3":{
+        "ru":"Снижение риска травматизма в повседневной жизни",
+        "en":"Reducing the risk of injury in everyday life",
+        "ua":"Зниження ризику травматизму у повсякденному житті"
+    },
+    "main-btn2":{
+        "ru":"Записаться",
+        "en":"Sign up for training",
+        "ua":"Записатись"
+    },
+
 }
