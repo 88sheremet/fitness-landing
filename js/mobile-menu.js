9c8234bbd7291d6fe7c88daa2fe2694af100aed2
Oleksandr Sheremet <o.sheremet@viseven.com>
2023-10-16 13:00:10 +0300

    update: add diploms window

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -26,4 +26,33 @@
 
 })();
 
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-diploms-open]'),
+    closeMenuBtn: document.querySelector('[data-diploms-close]'),
+    menu: document.querySelector('[data-diploms]'),
+    mainMob: document.querySelector('.main-mobile'),
+    programsMob: document.querySelector('.programs-mobile'),
+    aboutMob: document.querySelector('.about-mobile'),
+    questionsMob: document.querySelector('.questions-mobile'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.mainMob.addEventListener('click', check);
+  refs.programsMob.addEventListener('click', check);
+  refs.aboutMob.addEventListener('click', check);
+  refs.questionsMob.addEventListener('click', check);
+
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
+  function check(){
+    refs.menu.classList.remove('is-open');
+    
+  }
+
+})();
+
+
 
